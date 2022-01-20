b35a373f0eddcd30cc5ae411b5890cb9b9cce62b
awesomeayaan13 <41842116+awesomeayaan13@users.noreply.github.com>
2022-01-19 20:34:57 -0500

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,15 @@
+class Atm():
+    def __init__(self,cardNumber,pinNumber):
+        self.cardNumber=cardNumber
+        self.pinNumber=pinNumber
+
+
+    def withdrawMoney(self):
+        print("Enter the amount to withdraw:")
+    
+    def bankBalance(self, cardNumber, pinNumber):
+        return self.cardNumber
+
+        
+
+
